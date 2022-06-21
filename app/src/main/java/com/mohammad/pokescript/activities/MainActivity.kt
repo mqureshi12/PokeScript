7eae82ffc398674705986ab6e04b7ba0a300450e
Mohammad Qureshi <mqure016@fiu.edu>
2022-06-21 11:01:11 -0700

    created pokemon fragment layout file

--- app/src/main/java/com/mohammad/pokescript/activities/MainActivity.kt
+++ app/src/main/java/com/mohammad/pokescript/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mohammad.pokescript
+package com.mohammad.pokescript.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mohammad.pokescript.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
