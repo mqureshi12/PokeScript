5ed7d01d7c8b372ca3574671e3892b0ecd70794c
Mohammad Qureshi <mqure016@fiu.edu>
2022-06-22 10:54:37 -0700

    created Hilt class for dependency injection

---
+++ app/src/main/java/com/mohammad/pokescript/utilities/HiltAndroidApp.kt
@@ -0,0 +1,10 @@
+package com.mohammad.pokescript.utilities
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+// Class for dependency injection. First class to run in the app
+
+@HiltAndroidApp
+class HiltAndroidApp : Application() {
+}
