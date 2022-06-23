b86dba9d83b264f5943d85cda46d3df269c66cc2
Mohammad Qureshi <mqure016@fiu.edu>
2022-06-23 10:41:41 -0700

    created the app module object singleton

---
+++ app/src/main/java/com/mohammad/pokescript/injection/AppModule.kt
@@ -0,0 +1,46 @@
+package com.mohammad.pokescript.injection
+
+import android.content.Context
+import com.google.gson.Gson
+import com.mohammad.pokescript.api.ApiInterface
+import com.mohammad.pokescript.database.PokemonDB
+import com.mohammad.pokescript.database.PokemonDao
+import com.mohammad.pokescript.repository.MainRepository
+import com.mohammad.pokescript.repository.MainRepositoryImplement
+import com.mohammad.pokescript.utilities.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+// Holds all dependencies
+// Singleton - only one instance for dependencies throughout the app. Won't keep
+// recreating the db and api instance when accessing them
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providePokeApi() : ApiInterface = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        // Convert json data from the api responses into my data classes
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiInterface::class.java)
+
+    @Provides
+    fun providePokemonDAO(@ApplicationContext applicationContext: Context) : PokemonDao {
+        return PokemonDB.getDB(applicationContext)!!.pokemonDAO()
+    }
+
+    // Main repository
+    @Singleton
+    @Provides
+    fun providesMainRepository(api: ApiInterface, dao: PokemonDao) : MainRepository = MainRepositoryImplement(api, dao)
+}
