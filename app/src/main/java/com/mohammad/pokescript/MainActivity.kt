8b92f0b52aa7cfc11d7ef79b36e5884c60ef6dc0
Mohammad Qureshi <mqure016@fiu.edu>
2022-06-23 10:51:36 -0700

    figured out how to check in main activity for wifi or celluar connection and updated theme

--- app/src/main/java/com/mohammad/pokescript/MainActivity.kt
+++ app/src/main/java/com/mohammad/pokescript/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.mohammad.pokescript
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_PokeScript)
         setContentView(R.layout.activity_main)
+
+        if (!isNetworkConnected()) {
+            Toast.makeText(this, "No internet access if detected!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    // Check internet
+    private fun isNetworkConnected() : Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if(capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                return true
+            }
+        }
+        return false
     }
 }
