a79f508d80896d1dfcaf66e91a5c2fb9f6299bba
Mohammad Qureshi <mqure016@fiu.edu>
2022-06-24 11:56:04 -0700

    finished up listeners for the type filter dialog

--- app/src/main/java/com/mohammad/pokescript/FilterDialog.kt
+++ app/src/main/java/com/mohammad/pokescript/FilterDialog.kt
@@ -8,7 +8,7 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.DialogFragment
 
-class FilterDialog(var filterListener: FilterListener) : DialogFragment {
+class FilterDialog(var filterListener: FilterListener) : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +22,8 @@ class FilterDialog(var filterListener: FilterListener) : DialogFragment {
     ): View? {
 
         val fragmentContainer = container
-
         val rootview = layoutInflater.inflate(R.layout.dialog_filter, fragmentContainer, false)
+
         val fireImage = rootview.findViewById<ImageView>(R.id.fire)
         val bugImage = rootview.findViewById<ImageView>(R.id.bug)
         val darkImage = rootview.findViewById<ImageView>(R.id.dark)
@@ -42,11 +42,97 @@ class FilterDialog(var filterListener: FilterListener) : DialogFragment {
         val rockImage = rootview.findViewById<ImageView>(R.id.rock)
         val steelImage = rootview.findViewById<ImageView>(R.id.steel)
         val waterImage = rootview.findViewById<ImageView>(R.id.water)
+
         val cancelBtn = rootview.findViewById<Button>(R.id.DialogCancel)
 
         // Type click listeners
+        fireImage.setOnClickListener {
+            filterListener.typeToSearch("fire")
+            this.dismiss()
+        }
+        bugImage.setOnClickListener {
+            filterListener.typeToSearch("bug")
+            this.dismiss()
+        }
+        darkImage.setOnClickListener {
+            filterListener.typeToSearch("dark")
+            this.dismiss()
+        }
+        dragonImage.setOnClickListener {
+            filterListener.typeToSearch("dragon")
+            this.dismiss()
+        }
+        electricImage.setOnClickListener {
+            filterListener.typeToSearch("electric")
+            this.dismiss()
+        }
+        fairyImage.setOnClickListener {
+            filterListener.typeToSearch("fairy")
+            this.dismiss()
+        }
+        fightingImage.setOnClickListener {
+            filterListener.typeToSearch("fighting")
+            this.dismiss()
+        }
+        flyingImage.setOnClickListener {
+            filterListener.typeToSearch("flying")
+            this.dismiss()
+        }
+        ghostImage.setOnClickListener {
+            filterListener.typeToSearch("ghost")
+            this.dismiss()
+        }
+        grassImage.setOnClickListener {
+            filterListener.typeToSearch("grass")
+            this.dismiss()
+        }
+        groundImage.setOnClickListener {
+            filterListener.typeToSearch("ground")
+            this.dismiss()
+        }
+        iceImage.setOnClickListener {
+            filterListener.typeToSearch("ice")
+            this.dismiss()
+        }
+        normalImage.setOnClickListener {
+            filterListener.typeToSearch("normal")
+            this.dismiss()
+        }
+        poisonImage.setOnClickListener {
+            filterListener.typeToSearch("poison")
+            this.dismiss()
+        }
+        psychicImage.setOnClickListener {
+            filterListener.typeToSearch("psychic")
+            this.dismiss()
+        }
+        rockImage.setOnClickListener {
+            filterListener.typeToSearch("rock")
+            this.dismiss()
+        }
+        steelImage.setOnClickListener {
+            filterListener.typeToSearch("steel")
+            this.dismiss()
+        }
+        waterImage.setOnClickListener {
+            filterListener.typeToSearch("water")
+            this.dismiss()
+        }
 
+        cancelBtn.setOnClickListener {
+            this.dismiss()
+        }
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return rootview
+    }
+
+    interface FilterListener {
+        fun typeToSearch(type: String)
+    }
+
+    override fun onDestroy() {
+        // Retain instance of dialog fragment
+        if(dialog != null && retainInstance) dialog!!.setOnDismissListener(null)
+        super.onDestroy()
     }
 }
