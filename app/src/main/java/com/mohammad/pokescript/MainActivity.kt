33af194c3375f3af615b2cf2d6f8ae5b23860521
Mohammad Qureshi <mqure016@fiu.edu>
2022-06-29 15:58:18 -0700

    implemented background worker in main activity

--- app/src/main/java/com/mohammad/pokescript/MainActivity.kt
+++ app/src/main/java/com/mohammad/pokescript/MainActivity.kt
@@ -3,10 +3,22 @@ package com.mohammad.pokescript
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.work.BackoffPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import com.mohammad.pokescript.utilities.BackgroundWorker
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+
+private const val TAG = "MainActivity"
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -18,6 +30,45 @@ class MainActivity : AppCompatActivity() {
         if (!isNetworkConnected()) {
             Toast.makeText(this, "No internet access is detected!", Toast.LENGTH_LONG).show()
         }
+
+        workerCheck()
+
+        Log.d(TAG, "onCreate called")
+    }
+
+    // Setup background worker to periodically check for new pokemon and add it to my db
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun workerCheck() {
+        val preferences = getSharedPreferences("worker", MODE_PRIVATE)
+
+        // If worker value is not true then set the value and create the worker
+        if (preferences.getString("worker", "") != "cancel") {
+            val editPref = preferences.edit()
+            editPref.putString("worker", "enabled")
+            editPref.apply()
+            Log.d("BackgroundWorker", "SharedPreferences Boolean Set")
+
+            // Create worker constraints
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresBatteryNotLow(true)
+                .build()
+
+            // Run every 15 minutes
+            val backgroundWorker = PeriodicWorkRequest.Builder(BackgroundWorker::class.java, 15, TimeUnit.MINUTES)
+                .addTag("new_pokemon_checker")
+                .setConstraints(constraints)
+                .setBackoffCriteria(BackoffPolicy.EXPONENTIAL, 30, TimeUnit.SECONDS) // Back off in the event of a worker job failing
+                .build()
+
+            WorkManager.getInstance(this.applicationContext).enqueue(backgroundWorker)
+        } else {
+            cancelWorkers()
+        }
+    }
+
+    private fun cancelWorkers() {
+        WorkManager.getInstance(this).cancelAllWorkByTag("new_pokemon_checker")
     }
 
     // Check internet
